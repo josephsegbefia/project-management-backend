61a631c8f5e53c850d337f1122c0451452036fbd
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-01-23 16:00:26 +0000

    Make user changes

--- models/User.model.js
+++ models/User.model.js
@@ -4,7 +4,8 @@ const { Schema, model } = mongoose;
 const userSchema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  name: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
 });
 
 module.exports = model("User", userSchema);
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -11,10 +11,10 @@ const saltRounds = 10;
 
 // POST /auth/signup  - Creates a new user in the database
 router.post('/signup', (req, res, next) => {
-  const { email, password, name } = req.body;
+  const { email, password, firstName, lastName } = req.body;
 
-  // Check if email or password or name are provided as empty string 
-  if (email === '' || password === '' || name === '') {
+  // Check if email or password or name are provided as empty string
+  if (email === '' || password === '' || firstName === '' || lastName === '') {
     res.status(400).json({ message: "Provide email, password and name" });
     return;
   }
@@ -25,7 +25,7 @@ router.post('/signup', (req, res, next) => {
     res.status(400).json({ message: 'Provide a valid email address.' });
     return;
   }
-  
+
   // Use regex to validate the password format
   const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
   if (!passwordRegex.test(password)) {
@@ -48,16 +48,16 @@ router.post('/signup', (req, res, next) => {
       const hashedPassword = bcrypt.hashSync(password, salt);
 
       // Create the new user in the database
-      // We return a pending promise, which allows us to chain another `then` 
-      return User.create({ email, password: hashedPassword, name });
+      // We return a pending promise, which allows us to chain another `then`
+      return User.create({ email, password: hashedPassword, firstName, lastName });
     })
     .then((createdUser) => {
       // Deconstruct the newly created user object to omit the password
       // We should never expose passwords publicly
-      const { email, name, _id } = createdUser;
-    
+      const { email, firstName, lastName, _id } = createdUser;
+
       // Create a new object that doesn't expose the password
-      const user = { email, name, _id };
+      const user = { email, firstName, lastName, _id };
 
       // Send a json response containing the user object
       res.status(201).json({ user: user });
@@ -73,7 +73,7 @@ router.post('/signup', (req, res, next) => {
 router.post('/login', (req, res, next) => {
   const { email, password } = req.body;
 
-  // Check if email or password are provided as empty string 
+  // Check if email or password are provided as empty string
   if (email === '' || password === '') {
     res.status(400).json({ message: "Provide email and password." });
     return;
@@ -82,7 +82,7 @@ router.post('/login', (req, res, next) => {
   // Check the users collection if a user with the same email exists
   User.findOne({ email })
     .then((foundUser) => {
-    
+
       if (!foundUser) {
         // If the user is not found, send an error response
         res.status(401).json({ message: "User not found." })
@@ -94,13 +94,13 @@ router.post('/login', (req, res, next) => {
 
       if (passwordCorrect) {
         // Deconstruct the user object to omit the password
-        const { _id, email, name } = foundUser;
-        
+        const { _id, email, firstName, lastName } = foundUser;
+
         // Create an object that will be set as the token payload
-        const payload = { _id, email, name };
+        const payload = { _id, email, firstName, lastName };
 
         // Create and sign the token
-        const authToken = jwt.sign( 
+        const authToken = jwt.sign(
           payload,
           process.env.TOKEN_SECRET,
           { algorithm: 'HS256', expiresIn: "6h" }
