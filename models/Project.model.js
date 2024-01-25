b84ca53e1bb9b3cae7e34e43bfadf78ef04bff49
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-01-25 10:56:16 +0000

    Add user to project model

--- models/Project.model.js
+++ models/Project.model.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const projectSchema = new Schema({
+  user: {type: Schema.Types.ObjectId, ref: "User"},
   title: String,
   description: String,
   tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
--- models/User.model.js
+++ models/User.model.js
@@ -6,6 +6,7 @@ const userSchema = new Schema({
   password: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+  projects: [{ type: Schema.Types.ObjectId, ref: "Project"}]
 });
 
 module.exports = model("User", userSchema);
